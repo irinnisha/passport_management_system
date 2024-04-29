201efc25559b1c5bad6237653ecac7d460c6400c
Chessman-12 <mornob211001@bscse.uiu.ac.bd>
2024-04-29 19:54:39 +0600

    apply page 1 added

--- src/index.jsx
+++ src/index.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 import Signup from "./Signup";
 import User_dashboard from "./User_dashboard";
+import Apply_page_1 from "./Apply_page_1";
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
         <Route path="/" exact element={<Signup />} />
         <Route path="/User_dashboard" element={<User_dashboard />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/Apply_page_1" element={<Apply_page_1 />} />
       </Routes>
     </BrowserRouter>
   );
